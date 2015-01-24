a570e5c8dcb065ac7450e855ebd0bb91ebab639e
doordeduke <doordeduke@users.noreply.github.com>
2015-01-24 16:17:59 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of the matrix and retrieve the cached inversion / perform the 
+## inversion and return it
 
-## Write a short comment describing this function
+## Referenced Bill Hilton's "Making Sense of Assignment 2" thread available at
+## https://class.coursera.org/rprog-009/forum/thread?thread_id=457 as well as 
+## the instructions / example for this assignment
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function caches the inverse of the matrix
 
+makeCacheMatrix <- function(x = matrix()) { ## the input x will be a matrix
+      inv <- NULL ## inv is the inverse of our matrix and is reset whenever makeCacheMatrix is called
+      set <- function(y) {
+        x <<- y
+        inv <<- NULL
+      }
+      get <- function() x ## calls the input matrix
+      setinv <- function(solve) inv <<- solve ## called by cacheSolve when it is run the first time 
+                                          ## value of inv set (cached) using superassignment
+      getinv <- function() inv ## cached value of inv returned to cacheSolve
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix either by retrieving it from the cache (if available)
+## or solving for it (if not)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## x is created by makeCacheMatrix function
+      inv <- x$getinv() ## the inverse of x is retrieved 
+      if(!is.null(inv)) { ## if the value isn't NULL
+            message("Getting cache data") 
+            return(inv) ## return the cached value
+      }
+      data <- x$get() # if the inverse of x is NULL
+      inv <- solve(data, ...) # calculate the inversion
+      x$setinv(inv) # store the value of inv in makeCacheMatrix
+      inv # return the value
+
 }
